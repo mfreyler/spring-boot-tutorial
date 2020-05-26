f7c5b7930f3f37a38b770092480658759c2dbf2c
mfreyler <39602900+mfreyler@users.noreply.github.com>
2020-05-26 16:02:54 -0500

    added new Topic class and created a TopicController

--- src/main/java/io/mattfreyler/springboottutorial/topic/TopicController.java
+++ src/main/java/io/mattfreyler/springboottutorial/topic/TopicController.java
@@ -1,13 +1,21 @@
 package io.mattfreyler.springboottutorial.topic;
 
+import io.mattfreyler.springboottutorial.Topic;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 public class TopicController {
 
     @RequestMapping("/topics")
-    public String getAllTopics() {
-        return "All Topics";
+    public List<Topic> getAllTopics() {
+        return Arrays.asList(
+                new Topic("spring", "Spring Framework", "Spring Framework Description"),
+                new Topic("java", "Core Java", "Core Java Description"),
+                new Topic("javascript", "JavaScript", "JavaScript Description")
+                );
     }
 }
