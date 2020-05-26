8d06a72e0bd9397d710c515c3c506fee1bee185d
mfreyler <39602900+mfreyler@users.noreply.github.com>
2020-05-26 15:41:49 -0500

    first commit

---
+++ src/main/java/io/mattfreyler/springboottutorial/CourseApiApp.java
@@ -0,0 +1,13 @@
+package io.mattfreyler.springboottutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApiApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApiApp.class, args);
+	}
+
+}
---
+++ src/main/java/io/mattfreyler/springboottutorial/hello/HelloController.java
@@ -0,0 +1,13 @@
+package io.mattfreyler.springboottutorial.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String sayHi(){
+        return "Hi";
+    }
+}
